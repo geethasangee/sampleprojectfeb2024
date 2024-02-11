546b7a9a93ea86164193374a993d86c3e04716a6
geethasangee <sangeethadilli0504@gmail.com>
2024-02-11 12:57:56 +0530

    my first project

---
+++ src/org/sample/SampleTestProj.java
@@ -0,0 +1,23 @@
+package org.sample;
+
+public class SampleTestProj {
+
+	
+	private void sampleTest() {
+		System.out.println("Hello this is sample git project");
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
